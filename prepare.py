28f5d43b8c5c13782b899837a5804c454ddf67eb
hhzzff <2070620600@qq.com>
2024-10-09 22:40:06 +0800

    feat: :sparkles: added basic functions

---
+++ prepare.py
@@ -0,0 +1,158 @@
+import re
+import os
+import json
+import random
+import torch
+
+
+def read_txt_to_tensor(txt_path):
+    data_all = []
+    with open(txt_path, "r") as f:
+        for line in f:
+            line_data = line.strip()
+            if len(line_data) <= 30:
+                continue
+            data_num = re.findall(r"(-?\d+)", line_data[0 : len(line_data)])
+            data_num = [float(data_num[i]) for i in range(len(data_num))]
+            data_all.append(data_num)
+    return torch.tensor(data_all, dtype=torch.float32)
+
+def augment(
+    annot_json, target_data_num, max_seq_length, min_seq_length, more_than_one_word, READY_PATH
+):
+    seq_range = [i for i in range(min_seq_length, max_seq_length)]
+    READY_AUGMENTED_PATH = os.path.join(READY_PATH, "augment")
+    os.makedirs(READY_AUGMENTED_PATH, exist_ok=True)
+    augmented_annot_json = []
+    for i in range(target_data_num):
+        seq_length = random.choice(seq_range)
+        patch = random.sample(annot_json, seq_length)
+        data = torch.hstack([torch.load(j["path"]) for j in patch])
+        save_path = os.path.join(READY_AUGMENTED_PATH, f"{i}.pt")
+        torch.save(data, save_path)
+        label = []
+        for j in patch:
+            label += j["label"]
+        augmented_annot_json.append(
+            {"path": save_path, "label": label, "cls_label": more_than_one_word}
+        )
+
+    return augmented_annot_json
+
+
+def split_by_random(data):
+    N = len(data)
+    random.shuffle(data)
+    return data[: int(N * 0.8)], data[int(N * 0.8) :]
+
+def prepare(ROOT_PATH, TRAIN_FOLDER):
+    RAW_PATH = os.path.join(ROOT_PATH, r"train-data", TRAIN_FOLDER)
+    READY_PATH = os.path.join(ROOT_PATH, r"train-data\\ready")
+    os.makedirs(READY_PATH, exist_ok=True)
+    random.seed(42)
+    word_list = [
+        i
+        for i in os.listdir(RAW_PATH)
+        if len(i.split(" ")) == 1 and os.path.isdir(os.path.join(RAW_PATH, i))
+    ]
+    sentence_list = [
+        i
+        for i in os.listdir(RAW_PATH)
+        if len(i.split(" ")) > 1 and os.path.isdir(os.path.join(RAW_PATH, i))
+    ]
+
+    word2num_dict = {word: i + 1 for i, word in enumerate(word_list)}
+    word2num_dict["<blank>"] = 0
+    word2num_dict["<more_than_one_word>"] = len(word2num_dict)
+
+    num2word_dict = {i + 1:word for i, word in enumerate(word_list)}
+    num2word_dict[0] = "<blank>"
+    num2word_dict[len(num2word_dict)] = "<more_than_one_word>"
+
+    with open(os.path.join(READY_PATH, "word2num.json"), "w") as f:
+        json.dump(word2num_dict, f)
+
+    with open(os.path.join(READY_PATH, "num2word.json"), "w") as f:
+        json.dump(num2word_dict, f)
+
+    word_annot_json = []
+    for word in word_list:
+        RAW_WORD_PATH = os.path.join(RAW_PATH, word)
+        READY_WORD_PATH = os.path.join(READY_PATH, "word", word)
+        os.makedirs(READY_WORD_PATH, exist_ok=True)
+        counter = 0
+        for filename in os.listdir(RAW_WORD_PATH):
+            if "raw.txt" in filename:
+                data = read_txt_to_tensor(os.path.join(RAW_WORD_PATH, filename))
+                save_path = os.path.join(READY_WORD_PATH, f"{counter}.pt")
+                torch.save(data, save_path)
+                word_annot_json.append(
+                    {
+                        "path": save_path,
+                        "label": [word2num_dict[word]],
+                        "cls_label": word2num_dict[word],
+                    }
+                )
+                counter += 1
+
+
+    # sentence_annot_json = []
+    # for sentence in sentence_list:
+    #     RAW_SENTENCE_PATH = os.path.join(RAW_PATH, sentence)
+    #     READY_SENTENCE_PATH = os.path.join(READY_PATH, "sentence", sentence)
+    #     os.makedirs(READY_SENTENCE_PATH, exist_ok=True)
+    #     counter = 0
+    #     for filename in os.listdir(RAW_SENTENCE_PATH):
+    #         if "raw.txt" in filename:
+    #             data = read_txt_to_tensor(os.path.join(RAW_SENTENCE_PATH, filename))
+    #             save_path = os.path.join(READY_SENTENCE_PATH, f"{counter}.pt")
+    #             torch.save(data, save_path)
+    #             sentence_annot_json.append(
+    #                 {
+    #                     "path": save_path,
+    #                     "label": [
+    #                         word2num_dict[word.lower()]
+    #                         for word in re.split(r",|\s", sentence)
+    #                         if word
+    #                     ],
+    #                     "cls_label": word2num_dict["<more_than_one_word>"],
+    #                 }
+    #             )
+    #             counter += 1
+
+                
+
+    # augmented_annot_json = augment(
+    #     # word_annot_json + sentence_annot_json,
+    #     word_annot_json,
+    #     target_data_num=3000,
+    #     max_seq_length=20,
+    #     min_seq_length=10,
+    #     more_than_one_word=word2num_dict["<more_than_one_word>"],
+    #     READY_PATH=READY_PATH,
+    # )
+
+    word_train,word_test=split_by_random(word_annot_json)
+    with open(os.path.join(READY_PATH,'word','train.json'),'w') as f:
+        json.dump(word_train,f)
+    with open(os.path.join(READY_PATH,'word','test.json'),'w') as f:
+        json.dump(word_test,f)
+
+    # sent_train,sent_test=split_by_random(sentence_annot_json)
+    # with open(os.path.join(READY_PATH,'sentence','train.json'),'w') as f:
+    #     json.dump(sent_train,f)
+    # with open(os.path.join(READY_PATH,'sentence','test.json'),'w') as f:
+    #     json.dump(sent_test,f)
+
+    # augment_train,augment_test=split_by_random(augmented_annot_json)
+    # with open(os.path.join(READY_PATH,'augment','train.json'),'w') as f:
+    #     json.dump(augment_train,f)
+    # with open(os.path.join(READY_PATH,'augment','test.json'),'w') as f:
+    #     json.dump(augment_test,f)
+
+    with open(os.path.join(READY_PATH,'train.json'),'w') as f:
+        # json.dump(augment_train+word_train+sent_train,f)
+        json.dump(word_train,f)
+    with open(os.path.join(READY_PATH,'test.json'),'w') as f:
+        # json.dump(augment_test+word_test+sent_test,f)
+        json.dump(word_test,f)
---
+++ train.py
@@ -0,0 +1,106 @@
+import json
+import random
+import torch
+import torch.utils
+import torch.nn as nn
+import torch.optim as optim
+import os
+from tqdm import tqdm
+from dataset import NeckMpuDataset
+from model import Mpu2TextClassifier
+from torch.nn.utils.rnn import pad_sequence
+
+def batched_bincount(x, max_value, dim=2):
+    target = torch.zeros(
+        x.shape[0], x.shape[1], max_value + 1, dtype=x.dtype, device=x.device
+    )
+    values = torch.ones_like(x)
+    target.scatter_add_(dim, x, values)
+    return target
+
+
+def collate_fn(batch):
+    label = pad_sequence([i["label"] for i in batch], batch_first=True, padding_value=0)
+    data = pad_sequence(
+        [i["data"].transpose(0, 1) for i in batch], batch_first=True, padding_value=0
+    ).transpose(1, 2)
+    pad_length = (data.shape[2] // 20 + 1) * 20 - data.shape[2]
+    data = torch.cat(
+        [data, torch.zeros(size=(data.shape[0], data.shape[1], pad_length))], dim=-1
+    )
+    pad_mask = (data == 0)[:, 0, :].reshape(data.shape[0], -1, 20)
+    pad_mask = torch.argmax(
+        batched_bincount(pad_mask.to(torch.int64), max_value=1), dim=-1
+    )
+
+    return {
+        "data": data,
+        "label": label,
+        "cls_label": torch.hstack([i["cls_label"] for i in batch]),
+        "pad_mask": pad_mask,
+    }
+
+def train(ROOT_PATH, MODEL_PATH):
+
+    random.seed(42)
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    with open(os.path.join(ROOT_PATH, r"train-data\\ready\\word2num.json"), "r") as f:
+        word2num_dict = json.load(f)
+
+    num_epochs = 50
+    batch_size = 32
+
+
+
+
+    train_dataset = NeckMpuDataset(mode="train")
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn
+    )
+
+    model = Mpu2TextClassifier(
+        window_size=20,
+        mpu_channels=36,
+        n_dim=128,
+        n_head=4,
+        dropout=0.5,
+        cls_num=len(word2num_dict),
+        more_than_one_word=word2num_dict["<more_than_one_word>"],
+    ).to(DEVICE)
+
+    optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+
+    model.train()
+    loss_history = []
+    acc_history = []
+    for epoch in range(num_epochs):
+        mean_loss = 0.0
+        mean_acc = 0.0
+        with tqdm(train_loader, unit="batch") as tepoch:
+            for input_dict in tepoch:
+                for key in input_dict.keys():
+                    if isinstance(input_dict[key], torch.Tensor):
+                        input_dict[key] = input_dict[key].to(DEVICE, non_blocking=True)
+                tepoch.set_description(f"Epoch {epoch + 1}")
+                optimizer.zero_grad()
+                output_dict = model(input_dict)
+                loss = output_dict["loss"]
+                acc = output_dict["acc"]
+                mean_loss += loss.item()
+                mean_acc += acc.item()
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 3.0)
+                optimizer.step()
+                tepoch.set_postfix(acc=acc.item())
+
+        mean_loss /= len(train_loader)
+        mean_acc /= len(train_loader)
+        loss_history.append(mean_loss)
+        acc_history.append(mean_acc)
+
+    torch.save(model.state_dict(), MODEL_PATH)
+    torch.save(loss_history, "loss.pt")
+    torch.save(acc_history, "acc.pt")
